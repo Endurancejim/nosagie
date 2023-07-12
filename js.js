0597443eedeefb0dadff144125992b846554b739
Endurancejim <93596028+Endurancejim@users.noreply.github.com>
2023-07-12 13:51:14 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,26 @@
+
+const menuHamburger = document.querySelector(".hamburger")
+const navLinks = document.querySelector(".nav-links")
+
+menuHamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('open')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
